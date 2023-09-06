9b8c2d36f2dc49c2a8a7da85ab9c5e018d3c8641
danec <danejcamacho@gmail.com>
2023-09-06 12:35:39 -0700

    Update +page.svelte

--- facebook-clone/src/routes/+page.svelte
+++ facebook-clone/src/routes/+page.svelte
@@ -1,2 +1,127 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    let posts = [
+      {
+        id: 1,
+        author: 'Mike Hawk',
+        timestamp: '2 hours ago',
+        content: 'Hello, Svelte World!',
+        likes: 10,
+        comments: 5,
+      },
+      // Add more post objects here
+    ];
+  </script>
+
+<div class="header">
+    <div class="main-header">
+        
+        <div class="title">
+           Not Facebook 
+        </div>
+    </div>
+</div>
+
+
+
+<div class="feed">
+    {#each posts as post (post.id)}
+      <div class="post" key={post.id}>
+        <div class="post-header">
+          <div class="avatar"></div>
+          <div class="post-author">{post.author}</div>
+          <div class="timestamp">{post.timestamp}</div>
+        </div>
+        <div class="post-content">{post.content}</div>
+        <div class="post-actions">
+          <div class="action">
+            <i class="fas fa-thumbs-up like-icon"></i>
+            {post.likes} Likes
+          </div>
+          <div class="action">
+            <i  class="fas fa-comment comment-icon"></i>
+            {post.comments} Comments
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+
+<style>
+    /* Add your CSS styling here to make it look sleek */
+    .feed {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+  
+    .post {
+      background-color: #fff;
+      border: 1px solid #e0e0e0;
+      padding: 20px;
+      margin-bottom: 20px;
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    }
+  
+    .post-header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+    }
+  
+    .avatar {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background-color: #ccc;
+      margin-right: 10px;
+    }
+  
+    .post-author {
+      font-weight: bold;
+    }
+  
+    .timestamp {
+      color: #888;
+    }
+  
+    .post-content {
+      margin-bottom: 10px;
+    }
+  
+    .post-actions {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  
+    .action {
+      display: flex;
+      align-items: center;
+      color: #555;
+      cursor: pointer;
+      margin-right: 20px;
+    }
+  
+    .action i {
+      margin-right: 5px;
+    }
+  
+    .comment-icon {
+      color: #0078ff;
+    }
+  
+    .like-icon {
+      color: #ff4500;
+    }
+
+    .main-header{
+        background-color: #519df4;
+        padding: 2rem;
+    }
+
+    .title{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 4rem;
+    }
+  </style>
