bcbd5fecf727ad07da78ba24d3f2aa94ed8d4298
danec <danejcamacho@gmail.com>
2023-09-07 14:36:14 -0700

    added navbar

--- facebook-clone/src/routes/+layout.svelte
+++ facebook-clone/src/routes/+layout.svelte
@@ -7,6 +7,11 @@
            Not Facebook 
         </div>
     </div>
+    <div class="topnav">
+      <a class="active" href="/">Home</a>
+      <a href="/friends">Friends</a>
+      <a href="/about">About</a>
+    </div> 
 </div>
 
 
@@ -19,9 +24,36 @@
     }
 
     .title{
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 4rem;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-size: 3rem;
     }
+    /* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #59576c;
+  color: white;
+}
 </style>
 
 <slot></slot>
