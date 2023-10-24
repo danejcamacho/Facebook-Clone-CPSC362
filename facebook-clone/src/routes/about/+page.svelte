39d0c5434abe6681963820a8df304767f3c0884a
danec <danejcamacho@gmail.com>
2023-10-24 13:55:51 -0700

    added about content

--- facebook-clone/src/routes/about/+page.svelte
+++ facebook-clone/src/routes/about/+page.svelte
@@ -3,6 +3,30 @@
     export let data;
 </script>
 
-<div class ="">
+<h1>Who Are We?</h1>
+<p>
+    Here at Not Facebook, we pride ourselves on our extreme unoriginality
+    and blatant rehashing of a well known company. We're here to offer you the experience of a social media 
+    platform that, while being almost identical to what you're used to, assures 
+    you that we have a "totally different" identity. At Not Facebook, we believe 
+    in the power of imitation – because why be original 
+    when you don't have to? So, go ahead, create your account, and remember,
+    imitation isn't just flattery; it's the foundation of our social network
+</p>
 
-</div>
+
+<h1>What We Do</h1>
+<p>
+    Well, we do everything Facebook does, just not quite. At "Not Facebook," we
+     specialize in the art of being not quite Facebook. We bring you the latest in 
+     status updates, photo sharing, and even those mysterious friend requests, but
+      with a subtle twist of uniqueness. Our mission is to provide you with a platform
+       that's familiar yet refreshingly different. You can connect with friends, share 
+       your life's moments, and browse your news feed, just like you would on that other
+        platform...but not. We embrace the subtle irony of being "Not Facebook" by offering
+         you a space that feels like Facebook, only with a bit more quirk. So, whether 
+         you're tired of the same old or just fancy a change that's not too drastic, "Not 
+         Facebook" is here to offer you the "not-quite-the-same" social media experience you 
+         never knew you wanted. Join us in the quest for a Facebook that's not quite Facebook but
+          still something entirely unique!
+</p>
