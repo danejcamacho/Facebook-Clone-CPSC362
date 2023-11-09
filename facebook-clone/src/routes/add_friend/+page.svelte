51b112bba618dc36e4bf0e2a2e3bf8325393b3aa
danec <danejcamacho@gmail.com>
2023-11-09 13:15:13 -0800

    added add_friend page

---
+++ facebook-clone/src/routes/add_friend/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<h1> What is the name of your friend? </h1>
+<form>
+    <input type="text" id="fname" name="fname" ><br><br>
+</form>
