579680e57f87aff11caa7475a9443d54c38e348c
Josue <xJaramis@gmail.com>
2023-12-05 18:58:39 -0800

    Creating acc and login redirect to home page

--- facebook-clone/src/routes/create_account/+page.svelte
+++ facebook-clone/src/routes/create_account/+page.svelte
@@ -1,69 +1,67 @@
 <script>
-    let username = null;
-    let password = null;
+  let username = null;
+  let password = null;
 
-    const handleSubmit = () => {
-        if (username != null && password != null){
-            console.log(username,password)
-        }
+  const handleSubmit = () => {
+    if (username != null && password != null) {
+      console.log(username, password);
     }
-    
+  };
 </script>
 
 <main class="create-acc-struct">
-
-<!-- Body to explain website? idk -->
-  <div class='side-body'>
-    <h1 id='side-header'>Not really Facebook</h1>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam velit, reiciendis provident perferendis dicta officiis est in? Nemo blanditiis natus non praesentium recusandae aliquid commodi corporis, cum sapiente rem facere.
+  <!-- Body to explain website? idk -->
+  <div class="side-body">
+    <h1 id="side-header">Not really Facebook</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam velit, reiciendis
+    provident perferendis dicta officiis est in? Nemo blanditiis natus non praesentium
+    recusandae aliquid commodi corporis, cum sapiente rem facere.
   </div>
 
-<!-- Patch API Call at some point -->
-  <div class='form-body'>   
-    <h1 class='form-header'>Create Account!</h1>
+  <!-- Patch API Call at some point -->
+  <div class="form-body">
+    <h1 class="form-header">Create Account!</h1>
 
-      <form class="my-form" on:submit|preventDefault={handleSubmit}>
-
-        <div class="form-group">
-            <!-- svelte-ignore a11y-label-has-associated-control -->
-            <label class='form-labels'>Username: </label>
-            <input class='text-input' type="text" bind:value={username}>
-        </div>
-        
-        <div class="form-group">
-            <!-- svelte-ignore a11y-label-has-associated-control -->
-            <label class='form-labels'>Password: </label>
-            <input class='text-input' type="password" bind:value={password}>
-        </div>
-
-        <div class="form-group">
-            <label for="file-upload" class="pfp-file-upload">
-              Profile Picture (Optional)
-            </label>
-            <input id="file-upload" type="file"/>
-        </div>
-
-        <input class="button" type='submit' value="Create Account">
-      </form>
-
-
-      <div>
-        <a class='login-link' href="/login">Already have an account?</a>
+    <form class="my-form" action="http://localhost:5173/" method="get">
+      <div class="form-group">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="form-labels">Username: </label>
+        <input class="text-input" type="text" bind:value={username} />
       </div>
+
+      <div class="form-group">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="form-labels">Password: </label>
+        <input class="text-input" type="password" bind:value={password} />
+      </div>
+
+      <div class="form-group"></div>
+
+      <input
+        class="button"
+        type="submit"
+        value="Create Account"
+        on:click={handleSubmit()}
+      />
+    </form>
+
+    <div>
+      <a class="login-link" href="/login">Already have an account?</a>
+    </div>
   </div>
 </main>
 
 <style>
   .create-acc-struct {
-    color:#555555;
+    color: #555555;
     line-height: 2em;
     font-size: 18px;
     width: 85vw;
     padding-top: 2%;
-    margin:auto; 
+    margin: auto;
   }
 
-  .side-body{
+  .side-body {
     float: left;
     width: 54%;
     background-color: #333;
@@ -73,65 +71,57 @@
     font-size: xx-large;
   }
 
-  .form-body{
-    float:right;
-    width:40%;
+  .form-body {
+    float: right;
+    width: 40%;
     font-size: x-large;
   }
-  
-  #side-header{
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+  #side-header {
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   }
 
-/* .form-header{
+  /* .form-header{
   font-size: xx-large;
 } */
-.form-group{
-  padding: 8px;
-}
+  .form-group {
+    padding: 8px;
+  }
 
-.form-group label{
-  display: block;
-}
+  .form-group label {
+    display: block;
+  }
 
-.form-group input{
-  width: 100%;
-  height: 30px;
-}
+  .form-group input {
+    width: 100%;
+    height: 30px;
+  }
 
-.text-input{
-  font-size: smaller;
-}
+  .text-input {
+    font-size: smaller;
+  }
 
-.login-link{
-  font-size: medium;
-  color: black;
-}
+  .login-link {
+    font-size: medium;
+    color: black;
+  }
 
-.login-link:hover{
-  color: blue;
-}
+  .login-link:hover {
+    color: blue;
+  }
 
-.button{ 
-  background-color: #333; 
-   color: #ffffff; 
-   padding: 10px 15x; 
-   height: 40px;
-   float: right; 
-   width: 60%;
- } 
+  .button {
+    background-color: #333;
+    color: #ffffff;
+    padding: 10px 15x;
+    height: 40px;
+    float: right;
+    width: 60%;
+  }
 
-.button:hover{
-  background: #fff;
-  color: black;
-  cursor: pointer;
-}
-
-.pfp-file-upload{
-    border: 1px solid #333;
-    display: inline-block;
-    padding: 6px 12px;
+  .button:hover {
+    background: #fff;
+    color: black;
     cursor: pointer;
-}
-
+  }
 </style>
--- facebook-clone/src/routes/login/+page.svelte
+++ facebook-clone/src/routes/login/+page.svelte
@@ -1,46 +1,46 @@
 <script>
-    let username = null;
-    let password = null;
+  let username = null;
+  let password = null;
 
-    const handleSubmit = () => {
-        if (username != null && password != null){
-            console.log(username,password)
-        }
+  const handleSubmit = () => {
+    if (username != null && password != null) {
+      console.log(username, password);
     }
-
+  };
 </script>
 
-<center class='login-form'>
-    <body class='log-form-body'>
-        <h1>Login</h1>
-        
-        <form class='log-form-group' on:submit|preventDefault={handleSubmit}>
+<center class="login-form">
+  <body class="log-form-body">
+    <h1>Login</h1>
 
-            <div class="username">
-                <label for="user-input">Username</label>
-                <input class='user-input' type="text" bind:value={username} >
-            </div>
+    <form
+      class="log-form-group"
+      method="get"
+      action="http://localhost:5173/"
+    >
+      <div class="username">
+        <label for="user-input">Username</label>
+        <input class="user-input" type="text" bind:value={username} />
+      </div>
 
-            <div class="password">
-                <label for="pass-input">Password</label>
-                <input class="pass-input" type="password" bind:value={password}>
-            </div>
-            
-            <div>
-                <button class="log-but">Login</button>
-            </div>
-        </form>
-        
-        
-        <div class='create-acc'>
-            <a href="/create_account">Need an Account?</a>
-        </div>
-    </body>
+      <div class="password">
+        <label for="pass-input">Password</label>
+        <input class="pass-input" type="password" bind:value={password} />
+      </div>
+
+      <div>
+        <button class="log-but" on:submit|preventDefault={handleSubmit}>Login</button>
+      </div>
+    </form>
+
+    <div class="create-acc">
+      <a href="/create_account">Need an Account?</a>
+    </div>
+  </body>
 </center>
 
 <style>
-
-.login-form{
+  .login-form {
     background-color: #f4f4f4;
     padding-left: 5%;
     padding-right: 5%;
@@ -48,54 +48,53 @@
     line-height: 2em;
     height: 100vh;
     color: #555;
-}
+  }
 
-.create-acc a{
-    color:black;
-}
+  .create-acc a {
+    color: black;
+  }
 
-.create-acc a:hover{
+  .create-acc a:hover {
     color: blue;
     cursor: pointer;
-}
+  }
 
-.log-form-body{
+  .log-form-body {
     background-color: white;
-    float:none;
+    float: none;
     width: 40%;
     font-size: x-large;
     padding: 2pc;
     border: 3px;
     border-radius: 5%;
     border-style: outset;
-}
+  }
 
-.log-form-group label{
-    display:block;
+  .log-form-group label {
+    display: block;
     padding-right: 60%;
-}
+  }
 
-.log-form-group{
+  .log-form-group {
     padding: 8px;
-}
+  }
 
-.log-form-group input{
+  .log-form-group input {
     width: 80%;
     height: 35px;
-}
+  }
 
-.log-form-group button{
+  .log-form-group button {
     cursor: pointer;
-    background-color:#555;
+    background-color: #555;
     width: 60%;
     border-radius: 2%;
     color: white;
     padding: 5px 10px;
     height: 30px;
-    
-}
+  }
 
-.log-form-group button:hover{
+  .log-form-group button:hover {
     background-color: #1ab0ce;
-}
+  }
 </style>
